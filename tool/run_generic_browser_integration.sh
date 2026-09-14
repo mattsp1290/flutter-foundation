a85b872570ca8b43492c1197fceef032539d39a5
Matt Spurlin <mattsp1290@gmail.com>
2026-09-14 03:50:36 +0000

    fix: disable GPU for headless browser acceptance

--- tool/run_generic_browser_integration.sh
+++ tool/run_generic_browser_integration.sh
@@ -38,6 +38,7 @@ address=$(cat "$ready_file")
 
 cd "$root/examples/generic_ag_ui"
 timeout 600 "$flutter_bin" drive -d chrome --headless \
+  --web-browser-flag=--disable-gpu \
   --driver=test_driver/integration_test.dart \
   --target=integration_test/session_flow_test.dart \
   --dart-define="AG_UI_ENDPOINT=http://$address/generic/run"
